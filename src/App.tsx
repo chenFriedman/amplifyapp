cd3c8eb7a00862fb39fdf66c8701704503d5240b
Chen Friedman <chen.friedman11@gmail.com>
2022-06-08 21:52:14 +0300

    client

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BitcoinApp from './bitcoinApp/bitcoinApp';
+
+function App() {
+  return (
+    <div className="App">
+      <BitcoinApp/>
+    </div>
+  );
+}
+
+export default App;
